8a3977b503a5dd1adea772b8a701016d77c49b1b
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-07-08 20:45:13 +0600

    init app,подключил (bootstrap,axios,react-router-dom)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import {Routes} from "react-router-dom";
+
+const App = () => {
+
+
+  return (
+      <>
+          <Routes>
+
+          </Routes>
+          <h1 className="text-center">Hello</h1>
+      </>
+  )
+};
+
+export default App
